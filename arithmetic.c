db37f9cfddbc4e3d1d7bc2990dbbc2bebfb6c06e
Isaac Nealey <inealey@ucsd.edu>
2021-05-22 22:23:21 -0700

    added standalone example

---
+++ arithmetic.c
@@ -0,0 +1,11 @@
+//don't need these imports now but will prob use later
+#include <emscripten.h>
+#include <unistd.h>
+
+int doubler(int x) {
+  return 2 * x;
+}
+
+int incrementer(int x) {
+  return 1 + x;
+}
